ba9f1bb99f8ad973fb6628472ce097b5dd646f7d
Ike <student@epicodus.com>
2015-11-05 15:22:03 -0800

    move data to controller.

--- js/Controllers/MyController.js
+++
@@ -1,7 +0,0 @@
-function MyController($scope) {
-    $scope.emails = [
-        { from: 'John', subject:'I love angular', date: 'Jan 1'},
-        { from: 'Morticia', subject:'Something spooky', date: 'Oct 1'},
-        { from: 'Donald Trump', subject:'Blah blah blah', date: 'Nov 2'},
-    ]
-}
---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// var emailer = angular.module('emailer', []);
+//
+// emailer.controller('EmailController', ['$scope', function($scope) {
+//     EmailController($scope)
+//     $scope.emails = [
+//         { from: 'Google', subject:'I love angular', date: 'Jan 1'},
+//         { from: 'Morticia', subject:'Something spooky', date: 'Oct 1'},
+//         { from: 'Donald Trump', subject:'Blah blah blah', date: 'Nov 2'},
+//     ];
+// }
+// ]);
+// console.log(emails.from);
