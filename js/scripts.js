e54f9ab45563f1e6e47ba96084ec98212fd0e9f6
Ike Mattice <gogreen37@me.com>
2015-11-07 11:25:32 -0800

    add email popup when email is clicked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
-// var emailer = angular.module('emailer', []);
-//
-// emailer.controller('EmailController', ['$scope', function($scope) {
-//     EmailController($scope)
-//     $scope.emails = [
-//         { from: 'Google', subject:'I love angular', date: 'Jan 1'},
-//         { from: 'Morticia', subject:'Something spooky', date: 'Oct 1'},
-//         { from: 'Donald Trump', subject:'Blah blah blah', date: 'Nov 2'},
-//     ];
-// }
-// ]);
-// console.log(emails.from);
+var emailer = angular.module('emailer', []);
+
+emailer.controller('EmailController', ['$scope', function($scope) {
+    $scope.isPopupVisible = false;
+
+    $scope.showPopup = function (email) {
+        $scope.isPopupVisible = true;
+        $scope.selectedEmail = email;
+    };
+
+    $scope.closePopup = function() {
+        $scope.isPopupVisible = false;
+    };
+
+    $scope.emails = [
+        { from: 'Broodmother', subject:'Feeding Time', date: 'Jan 1', body:'My brood needs food.',},
+        { from: 'Axe', subject:'Axe hacks', date: 'Oct 1', body:'You\'re tougher than Axe thought!', },
+        { from: 'Dazzle', subject:'Dazzle!', date: 'Nov 2', body: 'Here comes the Shadow Priest.', },
+        { from: 'Winter Wyvern', subject:'On wings of ice', date: 'Nov 2', body: 'I will not abandon my friends to a world without winter', },
+    ];
+}
+]);
