a59bf06970100dc63768384db9dbd8a90485d12a
Ike Mattice <gogreen37@me.com>
2015-11-07 14:52:27 -0800

    add compose email framework.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 var emailer = angular.module('emailer', []);
 
 emailer.controller('EmailController', ['$scope', function($scope) {
+    $scope.emails = [
+        { from: 'Broodmother', subject:'Feeding Time', date: 'Jan 1', body:'My brood needs food.',},
+        { from: 'Axe', subject:'Axe hacks', date: 'Oct 1', body:'You\'re tougher than Axe thought!', },
+        { from: 'Dazzle', subject:'Dazzle!', date: 'Nov 2', body: 'Here comes the Shadow Priest.', },
+        { from: 'Winter Wyvern', subject:'On wings of ice', date: 'Nov 2', body: 'I will not abandon my friends to a world without winter', },
+    ];
+
     $scope.isPopupVisible = false;
+    $scope.isComposePopupVisible = false;
 
     $scope.showPopup = function (email) {
         $scope.isPopupVisible = true;
@@ -12,11 +20,11 @@ emailer.controller('EmailController', ['$scope', function($scope) {
         $scope.isPopupVisible = false;
     };
 
-    $scope.emails = [
-        { from: 'Broodmother', subject:'Feeding Time', date: 'Jan 1', body:'My brood needs food.',},
-        { from: 'Axe', subject:'Axe hacks', date: 'Oct 1', body:'You\'re tougher than Axe thought!', },
-        { from: 'Dazzle', subject:'Dazzle!', date: 'Nov 2', body: 'Here comes the Shadow Priest.', },
-        { from: 'Winter Wyvern', subject:'On wings of ice', date: 'Nov 2', body: 'I will not abandon my friends to a world without winter', },
-    ];
-}
-]);
+    $scope.showComposePopup = function() {
+        $scope.isComposePopupVisible = true;
+    };
+
+    $scope.closeComposePopup = function() {
+        $scope.isComposePopupVisible = false;
+    };
+}]);
