0f309e98fc41e3d5fe00aff0467fbc2df78c9722
Ike Mattice <gogreen37@me.com>
2015-11-07 22:34:20 -0800

    add forward and reply functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -2,37 +2,92 @@ var emailer = angular.module('emailer', []);
 
 emailer.controller('EmailController', ['$scope', function($scope) {
     $scope.emails = [
-        { from: 'Broodmother', subject:'Feeding Time', date: 'Jan 1', body:'My brood needs food.',},
-        { from: 'Axe', subject:'Axe hacks', date: 'Oct 1', body:'You\'re tougher than Axe thought!', },
-        { from: 'Dazzle', subject:'Dazzle!', date: 'Nov 2', body: 'Here comes the Shadow Priest.', },
-        { from: 'Winter Wyvern', subject:'On wings of ice', date: 'Nov 2', body: 'I will not abandon my friends to a world without winter', },
+        { to: 'me', from: 'Broodmother', subject:'Feeding Time', date: 'Jan 1', body:'My brood needs food.',},
+        { to: 'me', from: 'Axe', subject:'Axe hacks', date: 'Oct 1', body:'You\'re tougher than Axe thought!', },
+        { to: 'me', from: 'Dazzle', subject:'Dazzle!', date: 'Nov 2', body: 'Here comes the Shadow Priest.', },
+        { to: 'me', from: 'Winter Wyvern', subject:'On wings of ice', date: 'Nov 2', body: 'I will not abandon my friends to a world without winter', },
     ];
 
-    $scope.isPopupVisible = false;
-    $scope.isComposePopupVisible = false;
+    $scope.sentEmails = [
+        { to: 'Invoker', from: 'me', subject:'Mid lane', date: 'Nov 7', body: 'Bottom is missing! Careful!', },
+
+    ]
+
+    $scope.isEmailVisible = false;
+    $scope.isComposeEmailVisible = false;
     $scope.composeEmail = {};
     $scope.activeTab='inbox';
 
-    $scope.showPopup = function (email) {
-        $scope.isPopupVisible = true;
+    $scope.showEmail = function (email) {
+        $scope.isEmailVisible = true;
         $scope.selectedEmail = email;
     };
 
-    $scope.closePopup = function() {
-        $scope.isPopupVisible = false;
+    $scope.closeEmail = function() {
+        $scope.isEmailVisible = false;
     };
 
-    $scope.showComposePopup = function() {
+    $scope.showComposeEmail = function() {
         $scope.composeEmail = {};
-        $scope.isComposePopupVisible = true;
+        $scope.isComposeEmailVisible = true;
     };
 
-    $scope.closeComposePopup = function() {
-        $scope.isComposePopupVisible = false;
+    $scope.forward = function() {
+        $scope.isEmailVisible = false;
+        $scope.composeEmail = {};
+        //copy the data from the selectedEmail into composeEmail
+
+        angular.copy($scope.selectedEmail, $scope.composeEmail);
+
+        //edit the body and prefix it with a line and the original email information
+        $scope.composeEmail.body=
+        '\n------------------------------------------\n'+
+        'from:' + $scope.composeEmail.from + '\n' +
+        'sent:' + $scope.composeEmail.date + '\n' +
+        'to:' + $scope.composeEmail.to + '\n' +
+        'subject' + $scope.composeEmail.subject + '\n' +
+        $scope.composeEmail.body;
+
+        //prefix subject with "re:"
+        $scope.composeEmail.subject = 'FW:' + $scope.composeEmail.subject;
+        //the email is going to the person who sent it to us so populate the to field with the from field
+        $scope.composeEmail.to = '';
+        $scope.composeEmail.from = 'me';
+        $scope.isComposeEmailVisible = true;
+    };
+
+    $scope.reply = function() {
+        $scope.isEmailVisible = false;
+        $scope.composeEmail = {};
+        //copy the data from the selectedEmail into composeEmail
+
+        angular.copy($scope.selectedEmail, $scope.composeEmail);
+
+        //edit the body and prefix it with a line and the original email information
+        $scope.composeEmail.body=
+        '\n------------------------------------------\n'+
+        'from:' + $scope.composeEmail.from + '\n' +
+        'sent:' + $scope.composeEmail.date + '\n' +
+        'to:' + $scope.composeEmail.to + '\n' +
+        'subject' + $scope.composeEmail.subject + '\n' +
+        $scope.composeEmail.body;
+
+        //prefix subject with "re:"
+        $scope.composeEmail.subject = 'RE:' + $scope.composeEmail.subject;
+        //the email is going to the person who sent it to us so populate the to field with the from field
+        $scope.composeEmail.to = $scope.composeEmail.from;
+        $scope.composeEmail.from = 'me';
+        $scope.isComposeEmailVisible = true;
+    };
+
+    $scope.closeComposeEmail = function() {
+        $scope.isComposeEmailVisible = false;
     };
 
     $scope.sendEmail = function() {
-        $scope.isComposePopupVisible = false;
-        alert($scope.composeEmail.to + " " + $scope.composeEmail.subject + ' ' + $scope.composeEmail.body);
+        $scope.composeEmail.date = new Date();
+        $scope.composeEmail.from = 'me';
+        $scope.sentEmails.push($scope.composeEmail);
+        $scope.isComposeEmailVisible = false;
     };
 }]);
